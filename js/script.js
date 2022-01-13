f9aacfa77173cdd4acdeafd0f1f1a6b031912a68
Esteban Nahuel Carrizo <estebancarrizo619@gmail.com>
2022-01-12 22:04:39 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,55 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+  navbar.classList.toggle("activar")
+}
+
+document.querySelector("#acceder-btn").onclick = () => {
+  document.querySelector(".acceder-form-container").classList.toggle("activar");
+}
+
+document.querySelector("#close-acceder-form").onclick = () => {
+  document.querySelector(".acceder-form-container").classList.remove("activar");
+}
+
+window.onscroll = () => {
+  if(window.scrollY > 0){
+    document.querySelector(".site-header").classList.add("activar")
+  }else document.querySelector(".site-header").classList.remove("activar")
+
+  navbar.classList.remove("activar")
+}
+
+window.onload = () => {
+  if(window.scrollY > 0){
+    document.querySelector(".site-header").classList.add("activar")
+  }else document.querySelector(".site-header").classList.remove("activar")
+
+}
+
+var swiper = new Swiper(".vehiculo-slider", {
+  grabCursor: true,
+  centeredSlides: true,  
+  spaceBetween: 20,
+  loop:true,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
